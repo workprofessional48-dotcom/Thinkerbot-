c65417f79266080b2f3c69655940a640d3d51bac
workprofessional48-dotcom <workprofessional48@gmail.com>
2025-09-20 12:36:34 +0530

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-# BOT_TOKEN ko environment variable se read karo
-# Agar local run kar rahe ho aur env variable nahi hai, to fallback token use hoga
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8232368560:AAHd8IOXBBc_siTyi3NvUyxnlnN7ze-cPSQ")  # Yaha apna actual token daalna
